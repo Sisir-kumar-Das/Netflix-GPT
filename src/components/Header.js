48e07a5e2a3284c48cf9bd22017d48e5701ae82c
Sisir <das1234sisir@gmai.com>
2023-09-03 21:28:49 +0530

    redux toolkit added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/userSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(dispatch(removeUser()));
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className=" absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
@@ -14,7 +33,10 @@ const Header = () => {
           src="https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABa6Y5pM3JkNAfrT6Eft5tCbH2MaN5pMRxaSN-XFC50l-yFu-19V7MqIFhWgAHOjoJtIAKbO6fsH6rB-tfGr51sXpmjuwHHXthRKCvZhCX5UaEiwvxdC_RmBSh6Td1BWpH5ytu3r7t1hn8A.png?r=1d4"
           alt="user icon"
         />
-        <button className=" rounded-lg bg-red-700 text-white cursor-pointer font-bold px-4 mx-4 hover:bg-red-500">
+        <button
+          onClick={handleSignOut}
+          className=" rounded-lg bg-red-700 text-white cursor-pointer font-bold px-4 mx-4 hover:bg-red-500"
+        >
           Sign Out
         </button>
       </div>
