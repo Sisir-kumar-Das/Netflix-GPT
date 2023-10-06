0b6eafbb1ab3659bbe8531e9eaba252ed25eddcd
Sisir <das1234sisir@gmai.com>
2023-10-06 12:42:28 +0530

    movie slice created

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,20 +1,8 @@
-import React, { useEffect } from "react";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constant";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-  };
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
-
+  useNowPlayingMovies();
   return (
     <div>
       <Header></Header>
