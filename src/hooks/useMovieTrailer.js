25f448ea13c24d25cf88e446186bbb7b4a1b0e4a
Sisir <das1234sisir@gmai.com>
2024-01-20 22:55:22 +0530

    updated

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constant";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieList) => {
+  const dispatch = useDispatch();
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieList +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = !filterData ? json.results[0] : filterData[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+};
+
+export default useMovieTrailer;
