a78d311fed98e5733a21dda20b144b6b673d7700
Sisir <das1234sisir@gmai.com>
2024-03-02 21:09:55 +0530

    languange constant

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { LOGIN_BG } from "../utils/constant";
+import lang from "../utils/languageConstants";
 
 const gptSearchBar = () => {
   return (
     <>
       <div>
         <img className=" absolute w-full" src={LOGIN_BG} alt="Logo Pic" />
-        <div className=" relative  pt-[15%] w-full flex justify-center items-center">
+        <div className=" relative  pt-[15%] p-24 w-full flex justify-center items-center">
           <form className="  w-full ">
             <input
-              className="rounded-lg w-2/4 h-14  font-bold"
+              className="rounded-lg w-2/4 h-14 p-4 font-bold"
               type="text"
-              placeholder="What would you like to watch today!!"
+              placeholder={lang.odia.gptPlaceHolder}
             />
             <button className=" rounded-lg bg-red-700 text-white cursor-pointer font-bold px-4 mx-4 hover:bg-red-500 w-40 h-14">
-              Search
+              {lang.odia.search}
             </button>
           </form>
         </div>
---
+++ src/utils/languageConstants.js
@@ -0,0 +1,20 @@
+const lang = {
+  en: {
+    search: "Search",
+    gptPlaceHolder: "What would you like to watch today!!",
+  },
+  hindi: {
+    search: "खोज",
+    gptPlaceHolder: "आज आप क्या देखना चाहेंगे!!",
+  },
+  spanish: {
+    search: "BUSCAR",
+    gptPlaceHolder: "Que te gustaría ver hoy!!",
+  },
+  odia: {
+    search: " ସନ୍ଧାନ କର ",
+    gptPlaceHolder: "ଆଜି ଆପଣ କ’ଣ ଦେଖିବାକୁ ଚାହାଁନ୍ତି !!",
+  },
+};
+
+export default lang;
